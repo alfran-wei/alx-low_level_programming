70b5861b085b0386177365947ec564f53fd557e0
alfran-wei <alfranessone48@gmail.com>
2022-04-09 09:11:06 -0700

    add updat in task 100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-/*
+/**
  *main - print all combination of 2 digit number
- *but digit number are different
- *and '01' is the same combination of '10'
- *return : Always 0 (Success)
+ *but digits must be differents , no repeat (example 11),
+ *01 and 10 represent same thing
+ *
+ *return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-int i,n;
-i='0';
-n='1';
-while ( i < '9' )
+int i, n;
+i = '0';
+n = '1';
+while (i < '9')
 {
-while ( ( n > i ) && ( n <= '9' ) )
+while ((n > i)&&(n <= '9'))
 {
 putchar(i);
 putchar(n);
@@ -26,7 +27,7 @@ putchar(' ');
 n++;
 }
 i++;
-n=i+1;
+n = i + 1;
 }
 putchar('\n');
 return (0);
