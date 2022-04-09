08df6320290545f4ba4db9c7349b4bb9d214b593
alfran-wei <alfranessone48@gmail.com>
2022-04-09 09:44:26 -0700

    add task 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - print all combinations of three different digits
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int first;
+int ten;
+int hundred;
+
+for (third = '0'; third <= '9'; third++)
+{
+for (second = (third + 1); second <= '9'; second++)
+{
+for (first = (second + 1); first <= '9'; first++)
+{
+putchar(third);
+putchar(second);
+putchar(first);
+if (third != '7' || second != '8' || one != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+
+return (0);
+}
